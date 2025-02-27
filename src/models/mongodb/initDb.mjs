8573e4c59470d2a97438b3318ac59e77d0349da3
kyle <kroscoe45@gmail.com>
2025-02-27 05:37:45 -0800

    start fresh

--- src/models/mongodb/initDb.mjs
+++
@@ -1,6 +0,0 @@
-import {getDatabase} from './db.mjs'
-
-export default async function initDb() {
-  const db = await getDatabase()
-  await db.collection('tags').createIndex({trackId: 1, tag: 1}, {unique: true})
-}
